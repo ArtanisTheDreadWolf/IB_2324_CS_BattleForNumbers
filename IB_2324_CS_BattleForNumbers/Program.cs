1bc1e6bef178d14f43a8c99f350707166a5fd5de
24-0349C <24-0349C@CCL2-10>
2025-10-02 11:54:47 +0800

    I ACTUALLY GOT IT LFG

--- IB_2324_CS_BattleForNumbers/Program.cs
+++ IB_2324_CS_BattleForNumbers/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,14 +13,21 @@ namespace IB_2324_CS_BattleForNumbers
         // assuming all users follow instructions, no error correcting required
         static void Main(string[] args)
         {
-            Random rnd = new Random();
-            int[] nums = { };
-            int[] tempNum = { };
-            string uInput = "";
-            int iInput = 0;
-            int round = 0;
-            int fight = 0;
+            Run();
 
+        }
+        static void Run()
+        {
+            Random rnd = new Random(); // random number generator
+            int[] nums = { }; // numbers
+            int[] tempNum = { }; // temporary numbers
+            Gameplay();
+        }
+        static void Gameplay()
+        {
+           
+            string uInput = ""; // user input
+            int iInput = 0; // integer input
             while (true)
             {
                 Console.Write("Please enter how many contestants you would like to have (5-12) : ");
@@ -28,26 +36,35 @@ namespace IB_2324_CS_BattleForNumbers
                 if (iInput >= 5 && iInput <= 12)
                     break;
             }
-
-            nums = new int[iInput];
-
-            // putting random numbers in the whole num array
+            DisplayContestants(initializationofarray(iInput, new int[iInput]));
+            
+        }
+        static int[] initializationofarray(int iInput, int[] nums)
+        {
+            Random rnd = new Random();
             for (int x = 0; x < nums.Length; x++)
             {
                 // i just added a 100 number limit to make it easier
                 nums[x] = rnd.Next(100);
             }
-
-            // Displaying all contestants
+            return nums;
+        }
+        static void DisplayContestants(int[]nums)
+        {
             Console.Write("\nYour contestants! ");
-            for(int x = 0; x < nums.Length;x++)
+            for (int x = 0; x < nums.Length; x++)
             {
-                Console.Write($"{nums[x]}\t");
+                Console.Write($" {nums[x]}\t");
+               
             }
-
             Console.WriteLine("\n");
-
-            while(nums.Length > 1)
+            Battlefield6(nums, new int[] { });
+        }
+        static void Battlefield6(int[]nums, int[] tempNum)
+        {
+            int round = 0; // round counter
+            int fight = 0; // fight counter
+            while (nums.Length > 1)
             {
                 // example, if the length of nums = 7, the length of tempNum must be 4
                 if (nums.Length % 2 == 1)
@@ -57,13 +74,13 @@ namespace IB_2324_CS_BattleForNumbers
 
                 round++;
 
-                for(int x = 0; x < tempNum.Length; x++)
+                for (int x = 0; x < tempNum.Length; x++)
                 {
                     fight++;
 
                     if (x != nums.Length - 1 - x)
                     {
-                        Console.Write($"Round {round} Fight {fight} - {nums[x]} vs {nums[nums.Length - 1 - x]} winner : ");
+                        Console.Write($"Round {round} Fight {fight} - {nums[x]} vs {nums[nums.Length - 1 - x]} \nWinner : ");
                         if (nums[x] > nums[nums.Length - 1 - x])
                         {
                             tempNum[x] = nums[x];
@@ -84,11 +101,16 @@ namespace IB_2324_CS_BattleForNumbers
 
                 nums = tempNum;
                 Console.WriteLine();
+               
             }
-
+            WinnerMessage(nums);
+        }
+        static void WinnerMessage(int[]nums)
+        {
             Console.WriteLine($"End of competition! The winner is {nums[0]}!!! Congratulations!");
 
-            Console.ReadKey();
         }
+
     }
+    
 }
